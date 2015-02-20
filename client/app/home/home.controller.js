590fb4dc5aef82c4e2edda36136f9b5bda3195fa
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-19 20:50:12 -0600

    initial design

---
+++ client/app/home/home.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('chariotsApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('chariotsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      });
+  });
